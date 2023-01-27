e5c53ea51c239ab11a29549f03bbee754e0c8d93
vinigonz1993 <vinigonzalez1993@gmail.com>
2023-01-27 08:18:20 -0500

    Requesting https

--- frontend/src/components/requests/index.tsx
+++ frontend/src/components/requests/index.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default {
     oct: {
         get: () => axios.get(
-            'http://127.0.0.1:5000/oct',
+            'https://127.0.0.1:5000/oct',
             {
                 method: 'GET',
                 params: {
