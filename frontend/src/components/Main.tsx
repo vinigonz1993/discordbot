cc2fe2ed046155de2accd5c0d4085edb3e56fea7
vinigonz1993 <vinigonzalez1993@gmail.com>
2023-02-02 17:08:42 -0500

    Using navigate

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
-import styled from "styled-components";
+import { BtnPrimary } from "./common/Button";
 import Section from "./common/Section";
 import Title from "./common/Title";
-import OCT from "./OCT";
+import { useNavigate } from 'react-router-dom';
 
 const Main = () => {
+    const navigate = useNavigate();
     return (
         <>
             <Title title="ToolBoxAPIs"/>
             <Row>
                 <Col xl={6} lg={12} md={12} sm={12}>
                     <Section>
-                        OCTranstpo
-                        <hr/>
-                        <OCT />
+                        <BtnPrimary
+                            onClick={() => navigate('/o')}
+                        >
+                            OCTranspo
+                        </BtnPrimary>
                     </Section>
                 </Col>
                 <Col xl={6} lg={6} md={6} sm={12}>
